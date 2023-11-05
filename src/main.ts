e8027131ca8a67db67a5e943fc8c22a6e1048908
Eyuel Belete <eyuelbelete98@gmail.com>
2023-11-06 01:13:11 +0300

    added swagger and config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+   //configuration
+  const config: ConfigService = app.get(ConfigService);
+  const port: number = config.get<number>('PORT') || 7000;
+
+  //swagger configuration
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder()
+      .setTitle('NestJS API')
+      .setDescription('This is a nestjs starter api')
+      .addBearerAuth()
+      .build(),
+  );
+  SwaggerModule.setup('docs', app, document);
+
+
+  //root routes path
+  app.setGlobalPrefix('api');
+  const URL = config.get<string>('BASE_URL').replace(/\s/g, "")
+
+  await app.listen(port, () => {
+    console.log('URL = http://'+config.get<string>('BASE_URL')+":"+port+'/api');
+
+    console.log("Swagger = http://"+config.get<string>('BASE_URL')+":"+port+'/docs');
+  });
 }
 bootstrap();
